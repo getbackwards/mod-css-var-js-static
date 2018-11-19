dc6a61ad7dd43c8df5b1358693a3e834c147a73f
tgray <tony.gray@trademe.co.nz>
2018-11-20 09:43:30 +1300

    tidy up

--- src/js/mod-css-var-js.js
+++ src/js/mod-css-var-js.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('I work');
+// console.log('I work');
 
 const footer = document.querySelector('footer')
 const inputs = [].slice.call(document.querySelectorAll('input'));
@@ -8,7 +8,7 @@ const inputs = [].slice.call(document.querySelectorAll('input'));
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
 
-function handleUpdate(e) {
+function handleUpdate() {
   if (this.type === 'color') {
     footer.style.setProperty('--footer-color', this.value);
   } else {
