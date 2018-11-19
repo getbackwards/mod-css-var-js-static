62fde74e376513814f379ef6fe3350c76ee2681b
tgray <tony.gray@trademe.co.nz>
2018-11-20 09:26:43 +1300

    init

---
+++ src/js/mod-css-var-js.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log('I work');
+
+const footer = document.querySelector('footer')
+const inputs = [].slice.call(document.querySelectorAll('input'));
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+function handleUpdate(e) {
+  if (this.type === 'color') {
+    footer.style.setProperty('--footer-color', this.value);
+  } else {
+    footer.style.setProperty('--palatte-padding-left', this.value + 'px');
+  }
+}
